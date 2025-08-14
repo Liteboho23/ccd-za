6da71cd6084e618d63ff3e3bcb9b0498275a70e9
Liteboho <deemohapi470@gmail.com>
2025-08-14 15:57:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, send_from_directory, render_template, Response
 import os
 
 app = Flask(__name__)
@@ -48,5 +48,6 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
 
     app.run(host="0.0.0.0", port=port)
+
 
 
